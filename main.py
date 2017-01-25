3e8b41ccf039013b1ae979b9f090c4f84ea61db6
Andrew Bartelsmeyer <andrew.bartelsmeyer@gmail.com>
2017-01-24 22:25:43 -0600

    More or less final commit

--- main.py
+++ main.py
@@ -15,10 +15,42 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+def getRandomFortune():
+    fortunes = [
+        "The stars are going out. The judgement is nigh.",
+        "Hope is an illusion. Embrace your fear.",
+        "<h2>DEATH DEATH DEATH DEATH DEATH DEATH DEATH DEATH DEATH DEATH DEATH DEATH</h2>",
+        "A tall, dark stranger is in your future. He knows not the meaning of mercy",
+        "Flee! Hide! Alas, now it is too late.",
+        "We have so much more to fear than simply fear itself.",
+        "They know. They all know."
+        ]
+
+    index = random.randint(0,len(fortunes)-1)
+    return fortunes[index]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        header = "<h1>Fortune Cookie</h1>"
+
+        fortune = '<strong>' + getRandomFortune() + '</strong>'
+        fortune_sentence = "Your fortune is: " + fortune
+        fortune_paragraph = "<p>" + fortune_sentence + "</p>"
+
+        lucky_number = random.randint(1,100)
+        number_sentence = 'Your lucky number is <strong>' + str(lucky_number) + '</strong>'
+        if lucky_number = 69:
+            number_sentence = number_sentence + ' (nice)'
+        number_paragraph = "<p>" + number_sentence + "</p>"
+
+        cookie_again_button = "<a href='.'><button>Moar cookie plz</button></a>"
+
+        content = header + fortune_paragraph + number_paragraph + cookie_again_button
+
+        self.response.write(content)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
